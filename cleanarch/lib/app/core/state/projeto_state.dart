4b76b9ac5638d57889cf7359a5a266fe97656f99
Rodrigo Ferreira <43223244+Rodrigossff91@users.noreply.github.com>
2023-01-27 16:51:30 -0300

    Ajustes

--- cleanarch/lib/app/core/state/projeto_state.dart
+++ cleanarch/lib/app/core/state/projeto_state.dart
@@ -4,4 +4,15 @@ import 'package:get/get.dart';
 abstract class ProjetoState<S extends StatefulWidget, C extends GetxController>
     extends State<S> {
   C get controller => Get.find();
+
+  late final C controllerr;
+  @override
+  void initState() {
+    controllerr = Get.find<C>();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {});
+    onReady();
+    super.initState();
+  }
 }
+
+void onReady() {}
