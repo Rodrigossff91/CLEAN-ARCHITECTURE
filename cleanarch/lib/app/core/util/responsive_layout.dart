24d2d3cf4d5e1e627bfe99237180aa127cf073a9
rodrigoferreira <rodrigo@treeinova.com.br>
2023-04-04 16:13:08 -0300

    Inclusao

---
+++ cleanarch/lib/app/core/util/responsive_layout.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget? largeScreen;
+  final Widget? mediumScreen;
+  final Widget smallScreen;
+
+  const ResponsiveLayout(
+      {Key? key,
+      required this.largeScreen,
+      required this.mediumScreen,
+      required this.smallScreen})
+      : super(key: key);
+
+  static bool isSmallScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width < 800;
+  }
+
+  static bool isMediumScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width > 800 &&
+        MediaQuery.of(context).size.width < 1200;
+  }
+
+  static bool isLargeScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width > 800;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          return largeScreen ?? smallScreen;
+        } else if (constraints.maxWidth < 1200 && constraints.maxWidth > 800) {
+          return mediumScreen ?? smallScreen;
+        } else {
+          return smallScreen;
+        }
+      },
+    );
+  }
+}
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    required this.tablet,
+    required this.desktop,
+  }) : super(key: key);
+
+// This size work fine on my design, maybe you need some customization depends on your design
+
+  // This isMobile, isTablet, isDesktop helep us later
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 650;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1100 &&
+      MediaQuery.of(context).size.width >= 650;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1100;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      // If our width is more than 1100 then we consider it a desktop
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= 1100) {
+          return desktop;
+        }
+        // If width it less then 1100 and more then 650 we consider it as tablet
+        else if (constraints.maxWidth >= 650) {
+          return tablet;
+        }
+        // Or less then that we called it mobile
+        else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
+
+class ResponsiveLayoutt extends StatelessWidget {
+  final Widget mobileBody;
+  final Widget tabletBody;
+  final Widget desktopBody;
+
+  const ResponsiveLayoutt({
+    super.key,
+    required this.mobileBody,
+    required this.tabletBody,
+    required this.desktopBody,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 500) {
+          return mobileBody;
+        } else if (constraints.maxWidth < 1100) {
+          return tabletBody;
+        } else {
+          return desktopBody;
+        }
+      },
+    );
+  }
+}
